a747d91eb04995bc566445ec53541edfbf5c3a7c
devaraj <devaraj@inthreeaccess.com>
2020-04-04 12:13:54 +0530

    Lead list page added

---
+++ application/views/lead_list.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+   <meta charset="UTF-8">
+   <title> ::Lead Management:: </title>
+   <meta content='width=device-width,user-scalable=no' name='viewport'>
+   <link rel="shortcut icon" href="img/favicon.ico" />
+   <!-- global css -->
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/app.css" />
+
+   <!-- end of global css -->
+   <!--page level css -->
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/vendors/datatables/css/dataTables.bootstrap4.css" />
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/custom.css">
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/custom_css/datatables_custom.css">
+</head>
+<script>
+   var BASE_URL = "<?php echo base_url() ?>";
+</script>
+
+<body class="skin-default">
+   <div class="preloader">
+      <div class="loader_img"><img src="<?php echo base_url() ?>assets/img/loader.gif" alt="loading..." height="64" width="64"></div>
+   </div>
+   <!-- header logo: style can be found in header-->
+   <?php include('includes/header.php'); ?>
+   <div class="wrapper row-offcanvas row-offcanvas-left">
+      <?php include('includes/aside_left.php'); ?>
+      <?php include('includes/aside_right.php'); ?>
+      <!--section ends-->
+      <section class="content">
+         <!--main content-->
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="card ">
+                  <div class="card-header">
+                     <h3 class="card-title">
+                        <i class="ti-layout-grid3"></i> Lead List
+                     </h3>
+                     <span class="float-right">
+                        <i class="fa fa-fw ti-angle-up clickable"></i>
+                        <i class="fa fa-fw ti-close removecard"></i>
+                     </span>
+                  </div>
+                  <div class="card-body">
+                     <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" id="sample_1" style="width:100%">
+                           <thead>
+                              <tr>
+                                 <th>
+                                    Customer
+                                 </th>
+                                 <th>email</th>
+                                 <th>
+                                    Phone
+                                 </th>
+                                 <th>
+                                    Link Status
+                                 </th>
+                                 <th>Payment</th>
+                                 <th>Order date</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                               
+
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- /.content -->
+      </aside>
+      <!-- /.right-side -->
+   </div>
+   <!-- ./wrapper -->
+   <script src="<?php echo base_url() ?>assets/js/app.js" type="text/javascript"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/vendors/bootstrapvalidator/js/bootstrapValidator.min.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/vendors/sweetalert2/js/sweetalert2.min.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/js/custom_js/sweetalert.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/js/custom_js/app/common.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/js/custom_js/app/lead.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/vendors/datatables/js/jquery.dataTables.js"></script>
+   <script type="text/javascript" src="<?php echo base_url() ?>assets/vendors/datatables/js/dataTables.bootstrap4.js"></script>
+   <script>
+      "use strict";
+      $(document).ready(function() {  
+            var table = $('#sample_1').DataTable({
+            "dom": "<'row'<'col-md-5 col-12 float-left'l><'col-md-7 col-12'f>r><'table-responsive't><'row'<'col-md-5 col-12'i><'col-md-7 col-12'p>>", // datatable layout without  horizobtal scroll
+            "processing": true,
+            "serverSide": true,
+            "ajax": BASE_URL+"lead/lead_list",
+            "responsive": true
+         });
+ 
+
+      });
+   </script>
+</body>
+
+</html>
